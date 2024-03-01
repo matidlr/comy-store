81bb23e3ec298df54c6bf56ce3df1000a53d8e26
Matias <mati.delarosa1984@gmail.com>
2024-03-01 18:36:13 -0300

    Create pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
-
+import { 
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from "./pages"
 
 const App = () => {
   return (
