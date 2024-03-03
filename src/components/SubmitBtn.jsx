8755dc68e69ba99ab09f859299295128befaeca9
Matias <mati.delarosa1984@gmail.com>
2024-03-03 16:17:20 -0300

    Register page

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -1,9 +1,16 @@
-
+import { useNavigation } from "react-router-dom"
 
 const SubmitBtn = ({text}) => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting'
   return (
-    <button type='submit' className='btn btn-primary'>
-      {text}
+    <button type='submit' className='btn btn-primary' disabled={isSubmitting}>
+      {
+        isSubmitting? <>
+          <span className="loading loading-spinner"></span>
+          sending...
+        </>: text || 'submit'
+      }
     </button>
   )
 }
