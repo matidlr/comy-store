e37b36adddcacf8b52f436a43b17243362cefe3a
Matias <mati.delarosa1984@gmail.com>
2024-03-03 22:12:50 -0300

    About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,23 @@
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+          <h1 className="text-4xl font-bold leading-none tracking-tight 
+          sm:text-6xl">
+            We love
+          </h1>
+          <div className="stats bg-primary shadow">
+              <div className="stat-title text-primary-content text-4xl font-bold 
+              tracking-widest">
+                  comfy
+              </div>
+          </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        We sell the best products to solve our your needs
+      </p>
+    </>
   )
 }
 
