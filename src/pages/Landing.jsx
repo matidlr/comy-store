a57e786eacc8d622735365c57aad80d0f1ba7b27
Matias <mati.delarosa1984@gmail.com>
2024-03-05 12:24:19 -0300

    products

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,3 +1,4 @@
+import { FeaturedProducts } from "../components";
 import Hero from "../components/Hero";
 import { customFetch } from '../utils';
 const url = '/products?featured=true'
@@ -12,6 +13,7 @@ const Landing = () => {
   return (
     <>
      <Hero/>
+     <FeaturedProducts />
     </>
   )
 }
