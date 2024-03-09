78b35464b8012a537cb88790ba883ac618e50f28
Matias <mati.delarosa1984@gmail.com>
2024-03-09 17:57:45 -0300

    redux setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 import App from './App';
-
+import { ToastContainer } from 'react-toastify';
+import { store } from './store';
+import { Provider } from 'react-redux';
 
 
 
@@ -11,9 +13,8 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    
-      <App />
-  
-  </React.StrictMode>
+  <Provider store={store}>
+  <App />
+  <ToastContainer position='top-center' />
+</Provider>
 );
