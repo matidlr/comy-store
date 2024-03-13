7a7f061a0f13bbb7c87cc4be0cd95372bf13e4ef
Matias <mati.delarosa1984@gmail.com>
2024-03-13 16:05:17 -0300

    demo user

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { FormInput, SubmitBtn } from "../components"
-import {Form, Link} from 'react-router-dom';
+import {Form, Link, useNavigate} from 'react-router-dom';
 import { customFetch  } from "../utils";
 import { toast } from "react-toastify";
 import { loginUser } from "../features/user/userSlice";
@@ -21,6 +21,25 @@ export const action = (store) = async ({request}) => {
 };
 
 const Login = () => {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const loginAsGuestUser = async () =>{
+    try {
+      const response = await customFetch.post('/auth/local',{
+        identifier: 'test@test.com',
+        password: 'secret',
+      })
+      dispatch(loginUser(response.data))
+      toast.success('welcome guest user')
+      navigate('/')
+    } catch (error) {
+       console.log(error)
+       toast.error('guest user login error, please try again')
+    }
+  }
+
   return (
     <section className="h-screen grid place-items-center">
         <Form method='post' className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col
@@ -30,19 +49,20 @@ const Login = () => {
            type='email'
            label='email'
            name='identifier' 
-           defaultValue='test@test.com'
+           
            />
            <FormInput
            type='password'
            label='password'
            name='password' 
-           defaultValue='secret'
+           
            />
            <div className="mt-4">
              <SubmitBtn text='login' />
              
            </div>
-           <button type='button' className="btn btn-secondary btn-block" >
+           <button type='button' className="btn btn-secondary btn-block"
+             onClick={loginAsGuestUser} >
                guest user
              </button>
              <p className="text-center">
