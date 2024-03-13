0246bc127fdc7c7e2464e7216f5fbc205c3374c3
Matias <mati.delarosa1984@gmail.com>
2024-03-13 16:48:48 -0300

    checkout page

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,20 @@
-
+import { useSelector } from "react-redux";
+import { CheckoutForm, SectionTitle, cartTotals } from "../components";
+import CartTotals from "../components/CartTotals";
 
 const Checkout = () => {
-  return (
-    <div>Checkout</div>
-  )
+  const cartItems((state)=> state.cartState.cartTotal)
+  if(cartItems === 0){
+    return <SectionTitle text='Your car is empty' />
+  }
+  return <>
+  <SectionTitle text='place your order' />
+  <div className="mt-8 grid gap-8 md:grid-cols-2 items-start">
+    <CheckoutForm/>
+    <CartTotals/>
+  </div>
+  </>
+  
 }
 
 export default Checkout
