4e1822079e0a13d0ef1b8b65e5eb9c747d50c98e
Matias <mati.delarosa1984@gmail.com>
2024-03-13 15:21:11 -0300

    login user

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -6,6 +6,10 @@ const themes = {
   dracula : 'dracula',
 };
 
+const getUserFromLocalStorage = () => {
+  return JSON.parse(localStorage.getItem('user')) || null;
+}
+
 const getThemeFromLocalStorage = () =>{
   const theme = localStorage.getItem('theme') || themes.winter;
   document.documentElement.setAttribute('data-theme', theme);
@@ -14,7 +18,7 @@ const getThemeFromLocalStorage = () =>{
 
 
 const initialState= {
-  user:{username:'coding addict'},
+  user: getUserFromLocalStorage(),
   theme: getThemeFromLocalStorage(),
 }
 
@@ -23,7 +27,9 @@ const userSlice = createSlice({
   initialState,
   reducers:{
     loginUser:(state,action) =>{
-      console.log('login')
+      const user = {...action.payload.user, token:action.payload.jwt}
+      state.user = user
+      localStorage.setItem('user',JSON.stringify(user))
     },
     logoutUser:(state) =>{
       state.user = null
